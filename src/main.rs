25f9d09355565b06b252a55df250eef482e5b677
a08001212 <a080012122@gmail.com>
2023-02-23 22:27:18 +0800

    finish this project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+
+use rand::Rng;
+
+fn in_circle()->bool{
+    let mut rnd = rand::thread_rng();
+    let x = rnd.gen_range(0.0..1.0);
+    let y = rnd.gen_range(0.0..1.0);
+    if x*x + y*y < 1.0{
+        return true;
+    }
+
+    false
+}
+
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut count = 0;
+    let run_time = 1000000;
+    for _ in 0..run_time{
+        if in_circle(){
+            count += 1;
+        }
+    }
+
+    println!("Pi is {}", 4.0 * count as f64 / run_time as f64);
 }
